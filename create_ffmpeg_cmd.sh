cc1f9750713a6eda2a869672a8a8634315eb6e1e
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2018-12-13 10:55:23 -0300

    adding panic loglevel only to try to reduce stdout memory utilization

--- create_ffmpeg_cmd.sh
+++ create_ffmpeg_cmd.sh
@@ -5,5 +5,5 @@ OUTPUT_PATH=/tmp/stream/
 INPUT=$1
 OUTPUT=$2
 
-FFMPEG_CMD="ffmpeg -nostdin -rtsp_transport tcp -i ${INPUT} -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments ${OUTPUT_PATH}${OUTPUT}.m3u8 > /dev/null"
+FFMPEG_CMD="ffmpeg -loglevel panic -nostdin -rtsp_transport tcp -i ${INPUT} -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments ${OUTPUT_PATH}${OUTPUT}.m3u8"
 echo "${FFMPEG_CMD}"
