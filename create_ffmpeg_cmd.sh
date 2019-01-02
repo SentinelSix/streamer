578cbf73eab58d86cd45f07541ba243d198babf1
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2019-01-02 16:00:42 -0300

    test reconnection

--- create_ffmpeg_cmd.sh
+++ create_ffmpeg_cmd.sh
@@ -5,5 +5,5 @@ OUTPUT_PATH=/tmp/stream/
 INPUT=$1
 OUTPUT=$2
 
-FFMPEG_CMD="ffmpeg -loglevel panic -nostdin -rtsp_transport tcp -i ${INPUT} -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments ${OUTPUT_PATH}${OUTPUT}.m3u8"
+FFMPEG_CMD="ffmpeg -reconnect 1 -reconnect_delay_max 300 -loglevel panic -nostdin -rtsp_transport tcp -i ${INPUT} -f lavfi -i aevalsrc=0 -acodec aac -vcodec copy -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments ${OUTPUT_PATH}${OUTPUT}.m3u8"
 echo "${FFMPEG_CMD}"
